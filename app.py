a341292163eda2096ad7b77e5886f6228b017493
Aqo <aqotrooper@hotmail.com>
2018-09-14 17:15:23 +0300

    simple wsgi example

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-print('hey man')
-print('hi')
-print('blabla')
-print('howdy--etc')
-print('okay123--updates')
-print('newline!')
+from wsgiref.simple_server import make_server
+
+HOST = 'localhost'
+PORT = 8080
+
+def on_http_request(environment, set_response_head):
+	set_response_head('200 OK', [ ('Content-Type', 'text/plain') ])
+	return [ 'hello'.encode('utf-8') ]
+
+with make_server(HOST, PORT, on_http_request) as httpd:
+	print('listening on port ' + str(PORT))
+	httpd.serve_forever()
